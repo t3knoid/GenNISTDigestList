1a53d7f88f75a118ee721ac8b77f4f927394d841
Frank Refol <francis.refol@strozfriedberg.com>
2021-09-18 10:23:14 -0400

    Update CreateNuixNistDigestList.exe

--- GenNISTDigestList.bat
+++ GenNISTDigestList.bat
@@ -3,7 +3,7 @@ echo Download RDS minimal hash set archive start
 bin\wget --auth-no-challenge --no-check-certificate https://s3.amazonaws.com/rds.nsrl.nist.gov/RDS/current/rds_modernm.zip
 echo Download RDS minimal hash set archive complete
 echo Extract RDS minimal hash set archive start
-bin\7za.exe e %cd%\rds_modernm.zip -otarget -spe -mmt4
+bin\7za.exe e %cd%\rds_modernm.zip -otarget -spe -mmt4 -y
 echo Extract RDS minimal hash set archive complete
 echo Generate Nuix digest list start
 bin\CreateNuixNistDigestList.exe %cd%\target\NSRLFile.txt
